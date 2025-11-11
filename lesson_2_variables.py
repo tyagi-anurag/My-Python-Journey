1fb64b1c4fdb141957aa629bc9aa16b026b05f58
tyagi-anurag <anuragtyagi157@gmail.com>
2025-11-11 21:38:41 +0530

    i learned about variables and data types

---
+++ lesson_2_variables.py
@@ -0,0 +1,39 @@
+
+my_name = "Anurag"
+my_profession = "AI Engineer (in training)"
+
+
+print(f"My Name is {my_name} i am a {my_profession} ")
+
+
+
+my_age = 25 
+current_year = 2025
+
+
+birth_year = current_year - my_age
+print(f"my birth year is {birth_year}")
+
+
+
+bike_mileage = 65.5 
+petrol_price = 102.8
+
+
+cost_for_100km = (100 / bike_mileage) * petrol_price
+print(f"the cost for 100 km is {cost_for_100km}")
+
+
+
+is_learning = True
+is_expert = False
+
+print(f"Am I learning?: {is_learning}")
+print(f"Am I expert?: {is_expert}")
+
+
+
+print(f"the data type of 'my_name'is : {type(my_name)}")
+print(f"the data type of 'my_age' is: {type(my_age)}")
+print(f"the data type of 'bike_mileage' is: {type(bike_mileage)}")
+print(f"the data type of 'is_learning'is: {type(is_learning)}")
